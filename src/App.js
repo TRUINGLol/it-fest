5fe609fb1017026fe7c6af020f691b7a24b7fddc
Sergey Naibauer <sergeq@Truing.com>
2024-10-24 17:18:58 +0300

    feat: useNews hook in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter} from 'react-router-dom';
 import AppRouter from "./Components/route/AppRouter";
+import useNews from "./Hooks/useNews";
 
 export default function App(){
+  //Обькет для хранения состояния новостей
+  const [news,setNews] = useNews();
   return (
     <BrowserRouter>
-      <AppRouter/>
+      <AppRouter news={news} setNews={setNews}/>
     </BrowserRouter>
   );
 }
--- src/Hooks/useNews.jsx
+++ src/Hooks/useNews.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 export default function useNews(){
     const [news,setNews] = useState(()=>{
